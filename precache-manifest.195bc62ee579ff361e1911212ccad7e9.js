cec10931eec60b16894baaaf6fa50de5c698f209
David Stewart <darkorin00@hotmail.com>
2020-09-11 18:12:48 -0400

    react-deploy

---
+++ precache-manifest.195bc62ee579ff361e1911212ccad7e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402dbdc816ad8889cd999ff0dffb815c",
+    "url": "/Glitched-React/index.html"
+  },
+  {
+    "revision": "ae621f66243833c28eed",
+    "url": "/Glitched-React/static/css/main.ae6b566f.chunk.css"
+  },
+  {
+    "revision": "6fb30621ab3a4b0e99e1",
+    "url": "/Glitched-React/static/js/2.3f93bf16.chunk.js"
+  },
+  {
+    "revision": "23942446573592b3bea16e92c1916562",
+    "url": "/Glitched-React/static/js/2.3f93bf16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae621f66243833c28eed",
+    "url": "/Glitched-React/static/js/main.87a1a535.chunk.js"
+  },
+  {
+    "revision": "ae2853b2467f4a1fab22",
+    "url": "/Glitched-React/static/js/runtime-main.9e2d77bd.js"
+  },
+  {
+    "revision": "b78dbcbe46a85a96143a9e55a65fb8f7",
+    "url": "/Glitched-React/static/media/anna.b78dbcbe.jpg"
+  },
+  {
+    "revision": "5e56a067ff27a0a64a20a15a77bfcc3f",
+    "url": "/Glitched-React/static/media/background.5e56a067.png"
+  },
+  {
+    "revision": "2c1d3e9bdd24358f0c9448a046d4bd34",
+    "url": "/Glitched-React/static/media/default-av.2c1d3e9b.svg"
+  },
+  {
+    "revision": "72b409fa8e30f2bad4e3ebbee448e374",
+    "url": "/Glitched-React/static/media/errorpage.72b409fa.svg"
+  },
+  {
+    "revision": "8a631c2509762363fce4c95c555b07c5",
+    "url": "/Glitched-React/static/media/glitched-logo.8a631c25.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6944c34b9a22de0ae7de0fd37450628a.js"
+  "/Glitched-React/precache-manifest.195bc62ee579ff361e1911212ccad7e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Glitched-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
